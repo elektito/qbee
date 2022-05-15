54758b0f5eae1cbeeaec0a70971c060c176d1952
Mostafa Razavi <mostafa@sepent.com>
2022-05-15 23:56:18 +0200

    Fix code generation for binary operations

--- qbee/qvm_codegen.py
+++ qbee/qvm_codegen.py
@@ -40,20 +40,99 @@ class QvmCodeGen(BaseCodeGen, cg_name='qvm', code_class=QvmCode):
 
 @QvmCodeGen.generator_for(expr.NumericLiteral)
 def gen_num_literal(node, code, codegen):
-    code.add(('PUSH', node.type.type_char, node.value))
+    code.add((f'push{node.type.type_char}', node.value))
 
 
 @QvmCodeGen.generator_for(expr.BinaryOp)
 def gen_binary_op(node, code, codegen):
-    code.add(codegen.gen_code_for_node(node.left, code))
-    code.add(('possibly_conv',))
-    code.add(codegen.gen_code_for_node(node.right, code))
-    code.add(
-        ('possibly_conv',),
-        (node.op, node.type),
-        ('possibly_conv',),
-    )
+    Operator = expr.Operator
+    Type = expr.Type
+    if node.op in [Operator.ADD, Operator.SUB, Operator.MUL,
+                   Operator.DIV, Operator.EXP, Operator.NEG]:
+       # Convert both operands to the type of the resulting expression
+       left_type = node.type
+       right_type = node.type
+    elif node.op.is_comparison:
+        # Convert both operands to the bigger type of both
+        if node.left.type == Type.DOUBLE or \
+           node.right.type == Type.DOUBLE:
+            left_type = right_type = Type.DOUBLE
+        elif node.left.type == Type.SINGLE or \
+             node.right.type == Type.SINGLE:
+            left_type = right_type = Type.SINGLE
+        elif node.left.type == Type.LONG or \
+             node.right.type == Type.LONG:
+            left_type = right_type = Type.LONG
+        else:
+            left_type = right_type = Type.INTEGER
+    elif node.op == Operator.MOD or \
+         node.op.is_logical or \
+         node.op == Operator.INTDIV:
+        if node.type == Type.INTEGER:
+            left_type = Type.INTEGER
+            right_type = Type.INTEGER
+        else:
+            left_type = Type.LONG
+            right_type = Type.LONG
+    else:
+        raise InternalError(
+            'Unaccounted for binary operator: {node.op}')
 
+    codegen.gen_code_for_node(node.left, code)
+    if left_type != left_type:
+        from_char = node.type.type_char
+        to_char = left_type.type_char
+        code.add((f'conv{from_char}{to_char}',))
+
+    codegen.gen_code_for_node(node.right, code)
+    if right_type != node.right.type:
+        from_char = node.right.type.type_char
+        to_char = right_type.type_char
+        code.add((f'conv{from_char}{to_char}',))
+
+    if node.op.is_comparison:
+        # both operands are of the same type, so it doesn't matter we
+        # use which one here
+        op = f'sub{left_type.type_char}'
+        code.add((op,))
+
+        op = {
+            Operator.CMP_EQ: 'eq',
+            Operator.CMP_NE: 'ne',
+            Operator.CMP_LT: 'lt',
+            Operator.CMP_GT: 'gt',
+            Operator.CMP_LE: 'le',
+            Operator.CMP_GE: 'ge',
+        }[node.op]
+        code.add((op,))
+    else:
+        op = {
+            Operator.ADD: 'add',
+            Operator.SUB: 'sub',
+            Operator.MUL: 'mul',
+            Operator.DIV: 'div',
+            Operator.MOD: 'mod',
+            Operator.INTDIV: 'intdiv',
+            Operator.EXP: 'exp',
+            Operator.CMP_EQ: 'sub',
+            Operator.CMP_NE: 'sub',
+            Operator.CMP_LT: 'sub',
+            Operator.CMP_GT: 'sub',
+            Operator.CMP_LE: 'sub',
+            Operator.CMP_GE: 'sub',
+            Operator.NEG: 'neg',
+            Operator.PLUS: None, # noop
+            Operator.NOT: 'not',
+            Operator.AND: 'and',
+            Operator.OR: 'or',
+            Operator.XOR: 'xor',
+            Operator.EQV: 'eqv',
+            Operator.IMP: 'imp',
+        }[node.op]
+
+        if op is not None:
+            op += node.type.type_char
+            code.add((op,))
 
 @QvmCodeGen.generator_for(expr.UnaryOp)
 def gen_unary_op(node, code, codegen):
@@ -86,13 +165,13 @@ def gen_beep(node, code, codegen):
 @QvmCodeGen.generator_for(stmt.CallStmt)
 def gen_call(node, code, codegen):
     for arg in node.args:
-        code.add(codegen.gen_code_for_node(arg, code))
+        codegen.gen_code_for_node(arg, code)
     code.add(
-        ('PUSHARGSLEN', len(node.args)),
+        ('push%', len(node.args)),
         ('call', node.name),
     )
 
 
 @QvmCodeGen.generator_for(stmt.ClsStmt)
 def gen_cls(node, code, codegen):
-    code.add(('cls',))
+    code.add(('io', 'screen', 'cls'))
