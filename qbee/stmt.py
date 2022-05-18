a232200d81a191b28f7462d383450494a475af4a
Mostafa Razavi <mostafa@sepent.com>
2022-05-18 11:09:30 +0200

    Remove duplicate definition
    The abstract property children is already declared in the Node class,
    so there's no need for Stmt to declare it again.

--- qbee/stmt.py
+++ qbee/stmt.py
@@ -8,17 +8,6 @@ class Stmt(Node):
         for child in self.children:
             child.bind(compiler)
 
-    @property
-    @abstractmethod
-    def children(self):
-        # An implementation of this method should return all direct
-        # child expressions of this statement. It's important that
-        # this is properly implemented in all sub-classes, because the
-        # bind method (for both statements and expressions) uses it to
-        # bind a compiler to the expression which could be needed by
-        # some methods or properties.
-        pass
-
 
 class NoChildStmt(Stmt):
     @property
